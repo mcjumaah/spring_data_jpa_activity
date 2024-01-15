62553c18d9e5670da07b4c9b19eb1559b1ae1d34
mcjumaah <87046678+mcjumaah@users.noreply.github.com>
2024-01-15 13:31:10 +0800

    chore: Initialize Spring

---
+++ src/main/java/com/intelliseven/spring_data_jpa_activity/SpringDataJpaActivityApplication.java
@@ -0,0 +1,13 @@
+package com.intelliseven.spring_data_jpa_activity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaActivityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJpaActivityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/intelliseven/spring_data_jpa_activity/SpringDataJpaActivityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intelliseven.spring_data_jpa_activity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaActivityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
